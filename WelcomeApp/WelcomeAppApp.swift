91368d8568fd6d6a2dc8f993513fed7b8c97c049
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-29 16:40:54 +0700

    Initial Commit

---
+++ WelcomeApp/WelcomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeAppApp.swift
+//  WelcomeApp
+//
+//  Created by MacBook Air on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
