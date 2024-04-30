89aaf9bd231a4a2ac71c13f418aab25cffcd08a9
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-30 14:40:47 +0700

    first commit

---
+++ WelcomeApp/FeaturesCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturesCard.swift
+//  WelcomeApp
+//
+//  Created by MacBook Air on 29/04/24.
+//
+
+import SwiftUI
+
+struct FeaturesCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeaturesCard()
+}
---
+++ WelcomeApp/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  WelcomeApp
+//
+//  Created by MacBook Air on 29/04/24.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
